f2bbac0eac3064d478be28af3c57ea854283c3ff
zagorboda <zagorboda@gmail.com>
2020-10-20 22:12:25 +0300

    Convert str to int and store value in register

--- lab2/testread.asm
+++ lab2/testread.asm
@@ -7,6 +7,7 @@ DSEG SEGMENT PARA PUBLIC "DATA"
 	l db 0
 	is_negative db 0
 	msg db "Bad input$"
+    number dw 0
 DSEG ENDS
 
 CSEG SEGMENT PARA PUBLIC "CODE"
@@ -125,12 +126,36 @@ check_loop:
     lea di, dump
 	inc di
 	inc di
+    xor ax,ax
+    xor bx,bx
+    xor dx,dx
+    ; xor cx,cx
     jmp print_loop
 
 print_loop:
-    mov dl, [di]
-    mov ah, 02h
-    int 21h
+    mov bl, byte ptr [di]
+    ; mov ah, 02h
+    ; int 21h
+
+    ; sub dl, "0"
+    ; mov cl, byte [dump]
+    sub bx, '0'
+    ; mov dh, 0
+
+    push cx
+    push bx
+    push dx
+    call power_proc
+    pop dx
+    pop bx
+    pop cx
+
+    push dx
+    mul bx
+    pop dx
+    add dx, ax
+
+    ; mov ax, [dump]
 
     inc di
     dec cx
@@ -143,6 +168,9 @@ bad_input_label:
     jmp ending
 
 ending:
+    ; neg dx
+    ; xor ax, ax
+    ; sub ax, dx
     RET
     MAIN ENDP
 
@@ -151,6 +179,51 @@ bad_input_proc:
     MOV AH, 9
     INT 21H
     ret
+
+power_proc:
+    ; mov bl, l
+    ; sub bl, cl
+    ; mov cl, bl
+    xor bx, bx
+    mov bx, 10
+    xor ax, ax
+    mov ax, 1
+    
+    dec cx
+    je power_proc_ending
+    power:
+        mul bx
+        dec cl
+        jne power
+
+    power_proc_ending:
+        ret
+
+; digit_proc:
+
+; lea bx, dump
+; or bx, bx
+; jns m1
+; mov al, '-'
+; int 29h
+; neg bx
+; m1:
+;     mov ax, bx
+;     xor cx, cx
+;     mov bx, 10
+; m2:
+;     xor dx, dx
+;     div bx
+;     add dl, '0'
+;     push dx
+;     inc cx
+;     test ax, ax
+;     jnz m2
+; m3:
+;     pop ax
+;     int 29h
+;     loop m3
+;     ret
 
 CSEG ENDS
 END MAIN
