fba0446f22ff71f9c0f3927b7b87c1110dacc31a
zagorboda <zagorboda@gmail.com>
2020-12-17 15:48:07 +0200

    Search elems in 2d array

--- lab4/2d_arr.asm
+++ lab4/2d_arr.asm
@@ -2,17 +2,18 @@ stseg segment para stack "stack"
 	db 64 dup ("stack")
 stseg ends
 
-
 dseg segment para public "data"
 	number dw 0
-    array dw 10 dup("?")
+    array dw 50 dup("?")
     count dw ?
     iter dw 0
-    change_elem dw 0
-    first_elem_positive db 0
-    second_elem_positive db 0
 
-    input_arr_size db "Enter array size (from 1 up to 10): $"
+    input_msg_number_to_find db "Enter number to find $"
+    element_to_search dw 0
+    element_is_find dw 0
+    msg_elem_not_found db "Element not found$"
+    input_msg_number_of_rows db "Enter number of rows (from 1 to 50): $"
+    input_msg_number_of_columns db "Enter number of columns (from 1 to $"
     input_number_prompt db "Enter numbers from -32767 to 32767 $"
     input_number db "Enter number $"
 	msg_overflow db "Overflow $"
@@ -21,7 +22,8 @@ dseg segment para public "data"
     msg_sorted_arr db "Sorted array : $"
     msg_sum db "Sum is : $"
     error db 0
-    invalid_array_size db 'Invalid array size, must be in range from 1 to 10.$'
+    invalid_rows_number db 'Invalid number of rows, must be in range from 1 to 50$'
+    invalid_columns_number db 'Invalid number of columns, must be in range from 1 to $'
     msg_bad db "Bad input$"
     output_message db "Result = $"
 	dump db 7, ?, 7 dup('?')
@@ -32,6 +34,10 @@ dseg segment para public "data"
 
     sum dw 0
     max_elem dw 0
+
+    number_of_rows dw 0
+    number_of_columns dw 0
+    max_number_of_columns dw 0
 
 dseg ends
 
@@ -46,36 +52,63 @@ push ax
 mov ax,dseg
 mov DS,ax
 
-lea dx, input_arr_size
+
+lea dx, input_msg_number_of_rows
 mov ah, 9
 int 21H
 
-call read_int_proc
+call read_int_proc ; read number of rows
 cmp error, 1 ; check for invalid input (if put after next lines will print several error messages)
 je ending
 
 cmp number, 0 ; array max size
-jle invalid_array_size_main
+jle invalid_rows_number_main
 
-cmp number, 10
-jg invalid_array_size_main
+cmp number, 50
+jg invalid_rows_number_main
+
+mov bx, number
+mov number_of_rows, bx
 
 
-mov count, bx
-mov cx, count
-
-mov si,0 
-
-lea dx, input_number_prompt
+lea dx, input_msg_number_of_columns
 mov ah, 9
 int 21H
 
-mov dl, 10 ; print newline
-mov ah, 02h
-int 21h
-mov dl, 13
-mov ah, 02h
-int 21h ;---
+xor ax, ax
+xor dx, dx
+mov ax, 50
+div number_of_rows ; get max number of collumns , 50/rows
+mov bx, ax
+mov number, bx
+mov max_number_of_columns, bx
+call print_int_proc
+
+mov al, ')' ; print end of string
+int 29h
+mov al, ':'
+int 29h
+mov al, ' '
+int 29h
+
+call read_int_proc ; read number of columns
+cmp error, 1 ; check for invalid input (if put after next lines will print several error messages)
+je ending
+
+cmp number, 0 ; array max size
+jle invalid_columns_number_main
+
+mov bx, max_number_of_columns
+cmp number, bx
+jg invalid_columns_number_main
+
+mov bx, number
+mov number_of_columns, bx
+
+xor ax, ax
+mov ax ,number_of_rows
+mul number_of_columns
+mov cx, ax
 
 read_array:
     lea dx, input_number
@@ -84,13 +117,47 @@ read_array:
 
     push cx
 
-    xor bx, bx ; print element index
-    mov bx, si
-    sub bx, count
-    add bx, cx
-    inc bx
-    mov number, bx
+    mov al, ' '
+    int 29h
+
+    xor ax, ax
+    xor dx, dx
+    mov ax, si
+    mov bx, 2
+    div bx
+    mov bx, number_of_columns 
+    div bx
+    push dx
+    inc ax
+    mov number, ax
     call print_int_proc
+    pop dx
+
+    ; inc ax
+    ; mov number, ax
+    ; push dx
+    ; call print_int_proc
+    ; pop ax
+
+    mov al, ' '
+    int 29h
+
+    mov ax, dx
+    ; xor dx, dx
+    ; mov bx, 2
+    ; div bx
+
+    inc ax
+    mov number, ax
+    call print_int_proc
+
+    ; xor bx, bx ; print element index
+    ; mov bx, si
+    ; sub bx, count
+    ; add bx, cx
+    ; inc bx
+    ; mov number, bx
+    ; call print_int_proc
 
     mov al, ' '
     int 29h
@@ -108,159 +175,111 @@ read_array:
     add si, 2
     loop read_array
 
-mov si, 0
-xor ax, ax
-xor dx, dx
-mov cx, count
-sum_of_array:
-    mov ax, array[si]
 
-    add sum, ax
-    add dx, ax
-    jo call_overflow_sum
+search_element:
+    mov si, 0
 
-    add si, 2
-
-    loop sum_of_array       ; } while(--cx);
-
-    ; xor dx, dx
-    ; mov cx,count      
-    ; dec cx               ; outer loop iteration count
-    ; mov si, 0
-
-    lea dx, msg_sum
+    lea dx, input_msg_number_to_find
     mov ah, 9
     int 21H
 
-    mov dx, sum
-    mov number, dx
-    call print_int
+    call read_int_proc ; read number of rows
+    mov ax, number
+    mov element_to_search, ax
+    cmp error, 1 ; check for invalid input (if put after next lines will print several error messages)
+    je ending
 
-    jmp pre_find_max_elemet
-
-; nextcomp_sum:
-
-;     mov ax, array[si]
-
-;     add sum, ax
-;     add dx, ax
-
-;     add si, 2
-
-;     loop nextcomp_sum       ; } while(--cx)
-
-
-; mov number, dx
-; call print_int
-; jmp ending
-
-pre_find_max_elemet:
-    mov dl, 10 ; print newline
-    mov ah, 02h
-    int 21h
-    mov dl, 13
-    mov ah, 02h
-    int 21h ;---
-
-    mov si, 0
-    xor ax, ax
-    xor dx, dx
-    mov cx, count
-    mov ax, array[si]
-    mov max_elem, ax
-
-find_max_elemet:
-    mov ax, array[si]
-
-    cmp ax, max_elem
-    jg swap_greater
-    jmp noswap_greater
-
-    swap_greater:
-        xor max_elem, 0000
-        mov max_elem, ax
-
-    noswap_greater:
-        add si, 2
-        loop find_max_elemet
-
-lea dx, msg_max_elem
-mov ah, 9
-int 21H
-
-mov ax, max_elem
-mov number, ax
-call print_int_proc
-
-
-sort_array:
-    mov cx,count      
-    dec cx               ; outer loop iteration count
-
-    outerloop:                ; do {    // outer loop
-        mov bx, cx
-        mov si, 0 
-
-    innerrloop:
-
+    mov ax, number_of_rows
+    mov bx, number_of_columns
+    mul bx
+    mov cx, ax
+    search_loop:
+        mov iter, cx
         mov ax, array[si]
-        mov dx, array[si+2]
+        mov bx, element_to_search
+        cmp ax, bx
+        je print_finded_element
+        jmp no_match_search
 
-        cmp dx, ax
-        jg noswap
-        
-        swap:
-            mov array[si], dx
-            mov array[si+2], ax
+        print_finded_element:
+            mov al, ' '
+            int 29h
 
-    noswap: 
-        add si, 2
-        dec bx
-        jnz innerrloop
+            xor ax, ax
+            xor dx, dx
+            mov ax, si
+            mov bx, 2
+            div bx
+            mov bx, number_of_columns 
+            div bx
+            push dx
+            inc ax
+            mov number, ax
+            call print_int_proc
+            pop dx
 
-        loop outerloop       ; } while(--cx);
+            ; inc ax
+            ; mov number, ax
+            ; push dx
+            ; call print_int_proc
+            ; pop ax
 
-mov dl, 10 ; print newline
-mov ah, 02h
-int 21h
-mov dl, 13
-mov ah, 02h
-int 21h ;---
+            mov al, ' '
+            int 29h
 
-lea dx, msg_sorted_arr
-mov ah, 9
-int 21H
+            mov ax, dx
+            ; xor dx, dx
+            ; mov bx, 2
+            ; div bx
 
-xor cx,cx
-mov cx, count
-dec cx
-mov l, cl
-call print_array_proc
+            inc ax
+            mov number, ax
+            call print_int_proc
+
+            mov dl, 10 ; print newline
+            mov ah, 02h
+            int 21h
+
+            mov dl, 13
+            mov ah, 02h
+            int 21h ;---
+
+            mov element_is_find, 1
+
+        no_match_search:
+            mov cx, iter 
+            add si, 2
+            loop search_loop
+    
+    cmp element_is_find, 0
+    je print_elem_not_found
+    jmp ending
+
+print_elem_not_found:
+    lea dx, msg_elem_not_found
+    mov ah, 9
+    int 21H
 
 jmp ending
 
-call_overflow_sum:
-    mov error, 1
-    lea dx, msg_overflow
-    mov ah, 9
-    int 21H
-    jmp pre_find_max_elemet
-
-invalid_array_size_main:
-    lea dx, invalid_array_size
+invalid_rows_number_main:
+    lea dx, invalid_rows_number
     mov ah, 9
     int 21H
     jmp ending
 
-; array_size_less:
-;     lea dx, invalid_array_size
-;     mov ah, 9
-;     int 21H
-;     jmp ending
+invalid_columns_number_main:
+    lea dx, invalid_columns_number
+    mov ah, 9
+    int 21H
+    jmp ending
+
 
 ending:
     RET
     MAIN ENDP
+
+
 
 
 read_int_proc:
